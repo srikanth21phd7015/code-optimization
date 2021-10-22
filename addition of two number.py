881821a97d9b08a16b0e528abc4161343779966e
srikanth21phd7015 <92966739+srikanth21phd7015@users.noreply.github.com>
2021-10-22 09:45:56 +0530

    Add files via upload

---
+++ addition of two number.py
@@ -0,0 +1,12 @@
+import math
+def even(nu):#global space
+    n=nu#local space
+    if(n%2==0):
+        return n
+    else:
+        return 0
+    print("enter some element")
+    n=int(input())
+    if(even(n)!=0):
+        print("squre root",math.sqrt(x))
+
